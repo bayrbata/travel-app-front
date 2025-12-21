7ddb1bdf3daf70854bfa594a756ba3b68b2aaf26
temuulen <143782317+Gundegmaaa@users.noreply.github.com>
2025-12-21 17:50:51 +0800

    testaww

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -8,11 +8,13 @@
 
 import 'package:flutter_image_compress_web/flutter_image_compress_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   FlutterImageCompressWeb.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
